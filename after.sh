15ed405173abf2e6160a415d63ea7c27255edca6
Freek Van der Herten <freek@spatie.be>
2016-01-05 12:08:59 +0100

    Update after.sh

--- after.sh
+++ after.sh
@@ -56,22 +56,22 @@ if [ ! -f /usr/local/extra_homestead_software_installed ]; then
 
     # install elasticsearch (instructions copied from https://github.com/fideloper/Vaprobash/blob/master/scripts/elasticsearch.sh)	
     # Set some variables
-    #ELASTICSEARCH_VERSION=2.1.1 # Check http://www.elasticsearch.org/download/ for latest version
+    ELASTICSEARCH_VERSION=2.1.1 # Check http://www.elasticsearch.org/download/ for latest version
 
-    #sudo apt-get install -qq openjdk-7-jre-headless
+    sudo apt-get install -qq openjdk-7-jre-headless
 
-    #wget --quiet https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$ELASTICSEARCH_VERSION.deb
-    #sudo dpkg -i elasticsearch-$ELASTICSEARCH_VERSION.deb
-    #rm elasticsearch-$ELASTICSEARCH_VERSION.deb
+    wget --quiet https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$ELASTICSEARCH_VERSION.deb
+    sudo dpkg -i elasticsearch-$ELASTICSEARCH_VERSION.deb
+    rm elasticsearch-$ELASTICSEARCH_VERSION.deb
 
     # Configure Elasticsearch for development purposes (1 shard/no replicas, don't allow it to swap at all if it can run without swapping)
-    #sudo sed -i "s/# index.number_of_shards: 1/index.number_of_shards: 1/" /etc/elasticsearch/elasticsearch.yml
-    #sudo sed -i "s/# index.number_of_replicas: 0/index.number_of_replicas: 0/" /etc/elasticsearch/elasticsearch.yml
-    #sudo sed -i "s/# bootstrap.mlockall: true/bootstrap.mlockall: true/" /etc/elasticsearch/elasticsearch.yml
-    #sudo service elasticsearch restart
+    sudo sed -i "s/# index.number_of_shards: 1/index.number_of_shards: 1/" /etc/elasticsearch/elasticsearch.yml
+    sudo sed -i "s/# index.number_of_replicas: 0/index.number_of_replicas: 0/" /etc/elasticsearch/elasticsearch.yml
+    sudo sed -i "s/# bootstrap.mlockall: true/bootstrap.mlockall: true/" /etc/elasticsearch/elasticsearch.yml
+    sudo service elasticsearch restart
 
     # Configure to start up Elasticsearch automatically
-    #sudo update-rc.d elasticsearch defaults 95 10
+    sudo update-rc.d elasticsearch defaults 95 10
     
     #
     # remember that the extra software is installed
